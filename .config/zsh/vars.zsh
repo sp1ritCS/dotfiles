a5d21f2159de207dd6fc260c9fbc1fc263c7cbfa
sp1rit <sp1rit@disroot.org>
2020-11-03 10:21:57 +0100

    laptop changes

--- .config/zsh/vars.zsh
+++ .config/zsh/vars.zsh
@@ -1,36 +1,18 @@
-export ANDROID_HOME=/opt/android-sdk/
 export NPM_PACKAGES="${HOME}/.npm-packages"
 export NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
 
-export PATH="\
-/usr/lib/ccache/bin\
-:$PATH\
-:$HOME/.config/composer/vendor/bin\
-:$ANDROID_HOME/tools\
-:$ANDROID_HOME/tools/bin\
-:$ANDROID_HOME/platform-tools\
+export PATH="$PATH\
 :$NPM_PACKAGES/bin\
 :$HOME/.local/bin\
 "
-#export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/tools
-export PATH=$PATH:$ANDROID_HOME/tools/bin
-export PATH=$PATH:$ANDROID_HOME/platform-tools
-
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk/jre/
-
 if [[ -n $SSH_CONNECTION ]]; then
     export EDITOR="vim"
 else
-    export EDITOR="gvim"
+    export EDITOR="vim"
 fi
 
 export ZSH_AUTOSUGGEST_STRATEGY=(completion history)
 export ZSH_AUTOSUGGEST_USE_ASYNC=true
-
-if [ "$DESKTOP_SESSION" = "sway" ]; then
-    export $(gnome-keyring-daemon --start)
-fi
 
 RED="\e[31m"
 ORANGE="\e[33m"
